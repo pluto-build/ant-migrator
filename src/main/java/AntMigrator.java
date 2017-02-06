073a25a063f3a874d27361f8bb58061b1c52afc5
Manuel Weiel <manuel@weiel.eu>
2017-02-06 11:38:03 +0100

    basic cli parameter support for migrator

--- src/main/java/AntMigrator.java
+++ src/main/java/AntMigrator.java
@@ -1,4 +1,5 @@
 import generate.BuilderGenerator;
+import org.apache.commons.cli.*;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.ProjectHelper;
 import org.apache.tools.ant.Target;
@@ -14,8 +15,49 @@ import java.util.Map;
  */
 public class AntMigrator {
     public static void main(String[] args) throws Exception {
+        Options options = new Options();
+        options.addOption(Option.builder("h")
+                .longOpt("help")
+                .required(false)
+                .desc("Shows this message")
+                .build());
+        options.addOption(Option.builder("bf")
+                .longOpt("buildFile")
+                .required()
+                .hasArg()
+                .desc("The Buildfile to parse")
+                .build());
+        options.addOption(Option.builder("pkg")
+                .longOpt("package")
+                .required()
+                .desc("The java package name of the final pluto build script")
+                .hasArg()
+                .build());
+        options.addOption(Option.builder("noFD")
+                .longOpt("noFileDependencyDiscovery")
+                .desc("Switches off file dependency discovery (not recommended).")
+                .build());
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine line;
+        try {
+            // parse the command line arguments
+            line = parser.parse(options, args);
+
+            if (line.hasOption("h")) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("ant2pluto", options);
+            }
+        } catch (ParseException exp) {
+            System.out.println("Unexpected exception:" + exp.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("ant2pluto", options);
+            return;
+        }
+
+
         Project project = new Project();
-        File buildFile = new File(args[0]);
+        File buildFile = new File(line.getOptionValue("bf"));
         project.init();
         ProjectHelper.configureProject(project, buildFile);
 
@@ -23,7 +65,7 @@ public class AntMigrator {
         Map<String, String> targets = new HashMap<>();
         for (Target target : project.getTargets().values()) {
             if (!target.getName().isEmpty()) {
-                BuilderGenerator generator = new BuilderGenerator("build.pluto.plutoanttester", target.getName() + "Builder", false);
+                BuilderGenerator generator = new BuilderGenerator(line.getOptionValue("pkg"), target.getName() + "Builder", !line.hasOption("noFD"));
                 generator.setCommands(Arrays.asList(target.getTasks()));
                 generator.setDependentBuilders(Collections.list(target.getDependencies()));
                 targets.put(target.getName(), generator.getPrettyPrint());
