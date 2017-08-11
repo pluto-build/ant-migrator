84a3dc46411f5dd228eb075f74caa10f7b325514
Manuel Weiel <manuel@weiel.eu>
2017-08-11 02:29:48 +0200

    updated tests

--- src/test/java/antplutomigrator/correctness/asmtools/AsmToolsCorrectnessWithDockerTest.java
+++ src/test/java/antplutomigrator/correctness/asmtools/AsmToolsCorrectnessWithDockerTest.java
@@ -1,8 +1,7 @@
-package antplutomigrator.correctness.slick;
+package antplutomigrator.correctness.asmtools;
 
 import antplutomigrator.testrunners.comparison.*;
 import antplutomigrator.testrunners.utils.TaskExecutor;
-import antplutomigrator.testrunners.utils.TestTask;
 import antplutomigrator.testrunners.utils.tasks.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -21,31 +20,34 @@ import java.util.List;
 /**
  * Created by manuel on 01.06.17.
  */
-public class SlickCorrectnessWithDockerTest {
-    private Log log = LogFactory.getLog(SlickCorrectnessWithDockerTest.class);
+public class AsmToolsCorrectnessWithDockerTest {
+    private Log log = LogFactory.getLog(AsmToolsCorrectnessWithDockerTest.class);
 
     private boolean debug = false;
 
-    private String projectName = "slick";
-    private String projectSourceName = "slick";
-    private String projectMainName = "Slick";
-    private String md5Hash = "203a24252a1e18d1b52b1d7f27d10a34";
+    private String projectName = "asmtools";
+    private String projectSourceName = "asmtools-00021c8bcd23";
+    private String projectMainName = "Asmtools";
+    private String md5Hash = "b076821cb9a010dc4b12f7449b2be7b2";
 
-    URL url = new URL("http://slick.ninjacave.com/slick.zip");
+    URL url = new URL("http://hg.openjdk.java.net/code-tools/asmtools/archive/00021c8bcd23.zip");
 
     File zipFile = new File("../migrator-testdata/antplutomigrator/downloads/"+projectName+".zip");
 
     File testDir = new File("../migrator-testdata/antplutomigrator/correctness/"+projectName+"fd/");
     File sourceDir = new File(testDir,"source");
     File antDir = new File(testDir, "ant");
-    File antBuildXml = new File(antDir, projectSourceName +"/build.xml");
+    File antBuildXml = new File(antDir, projectSourceName +"/build/build.xml");
     File plutoDir = new File(testDir, "pluto");
-    File plutoBuildXml = new File(plutoDir, projectSourceName +"/build.xml");
-    File targetDir = new File(plutoDir, "target");
     File antSrcDir = new File(antDir, projectSourceName);
     File plutoSrcDir = new File(plutoDir, projectSourceName);
+    File antBuildSrcDir = new File(antSrcDir, "build");
+    File plutoBuildSrcDir = new File(plutoSrcDir, "build");
+    File plutoBuildXml = new File(plutoBuildSrcDir, "build.xml");
+    File targetDir = new File(plutoDir, "target");
 
-    public SlickCorrectnessWithDockerTest() throws MalformedURLException {
+
+    public AsmToolsCorrectnessWithDockerTest() throws MalformedURLException {
     }
 
     @Test
@@ -54,20 +56,9 @@ public class SlickCorrectnessWithDockerTest {
 
         taskExecutor.addTask(new DeleteDirTask(testDir));
         taskExecutor.addTask(new ProvideDownloadTask(url, md5Hash, zipFile));
-        taskExecutor.addTask(new UnzipTask(zipFile, new File(antDir, "slick")));
-        taskExecutor.addTask(new TestTask() {
-            @Override
-            public String getDescription() {
-                return "Create examples dir";
-            }
-
-            @Override
-            public void execute() throws Exception {
-                Files.createDirectories(new File(antSrcDir, "examples").toPath());
-            }
-        });
+        taskExecutor.addTask(new UnzipTask(zipFile, antDir));
         taskExecutor.addTask(new CopyDirectoryTask(antSrcDir, plutoDir));
-        MigrateAntToPlutoTask migrateAntToPlutoTask = new MigrateAntToPlutoTask(plutoBuildXml, plutoDir, "build.pluto."+projectName, true, debug, Arrays.asList("dist"));
+        MigrateAntToPlutoTask migrateAntToPlutoTask = new MigrateAntToPlutoTask(plutoBuildXml, plutoDir, "build.pluto."+projectName, true, debug);
         migrateAntToPlutoTask.setContinueOnError(true);
         migrateAntToPlutoTask.setCalculateStatistics(true);
         taskExecutor.addTask(migrateAntToPlutoTask);
@@ -82,7 +73,7 @@ public class SlickCorrectnessWithDockerTest {
         mounts.add(new Mount(antDir, new File("/share/test/")));
         mounts.add(new Mount(new File(System.getProperty("user.home")+"/.m2/"), new File("/share/m2/")));
 
-        taskExecutor.addTask(new DockerRunnerTask(antDir, projectName+"_Ant", new String(Files.readAllBytes(Paths.get(this.getClass().getResource("ant_command.txt").toURI()))), new File("/share/test/"+projectName+"/"), mounts));
+        taskExecutor.addTask(new DockerRunnerTask(antDir, projectName+"_Ant", new String(Files.readAllBytes(Paths.get(this.getClass().getResource("ant_command.txt").toURI()))), new File("/share/test/"+projectSourceName+"/build/"), mounts));
 
 
         mounts = new ArrayList<>();
@@ -93,20 +84,13 @@ public class SlickCorrectnessWithDockerTest {
 
         String plutoRunCommand = new String(Files.readAllBytes(Paths.get(this.getClass().getResource("pluto_run_command.txt").toURI())));
         plutoRunCommand = CompileJavaTask.substituteVars(plutoRunCommand, new String[] {"<classpath>", "<projectname>", "<projectmain>"}, new String[]{classPathDocker, projectName, projectMainName});
-        taskExecutor.addTask(new DockerRunnerTask(plutoDir, projectName+"_Pluto", plutoRunCommand, new File("/share/test/"+projectName+"/"), mounts));
+        taskExecutor.addTask(new DockerRunnerTask(plutoDir, projectName+"_Pluto", plutoRunCommand, new File("/share/test/"+projectSourceName+"/build/"), mounts));
 
-
-        ComparerTask comparerTask = new ComparerTask(new File(antSrcDir, "target"), new File(plutoSrcDir, "target"));
+        ComparerTask comparerTask = new ComparerTask(new File(antDir, "asmtools-6.0-build"), new File(plutoDir, "asmtools-6.0-build"));
         comparerTask.getDirectoryComparer().addFileComparer(new MD5FileComparer());
         comparerTask.getDirectoryComparer().addFileComparer(new UnzipFileComparer(comparerTask.getDirectoryComparer()));
         comparerTask.getDirectoryComparer().addFileComparer(new LineByLineFileComparer(Arrays.asList(new EqualLineComparer(), new JavaDocDateIgnoredLineComparer())));
         taskExecutor.addTask(comparerTask);
-
-        ComparerTask distComparerTask = new ComparerTask(new File(antSrcDir, "dist"), new File(plutoSrcDir, "dist"));
-        distComparerTask.getDirectoryComparer().addFileComparer(new MD5FileComparer());
-        distComparerTask.getDirectoryComparer().addFileComparer(new UnzipFileComparer(distComparerTask.getDirectoryComparer()));
-        distComparerTask.getDirectoryComparer().addFileComparer(new LineByLineFileComparer(Arrays.asList(new EqualLineComparer(), new JavaDocDateIgnoredLineComparer())));
-        taskExecutor.addTask(distComparerTask);
 
         taskExecutor.executeTasks();
     }
@@ -117,20 +101,9 @@ public class SlickCorrectnessWithDockerTest {
 
         taskExecutor.addTask(new DeleteDirTask(testDir));
         taskExecutor.addTask(new ProvideDownloadTask(url, md5Hash, zipFile));
-        taskExecutor.addTask(new UnzipTask(zipFile, new File(antDir, "slick")));
-        taskExecutor.addTask(new TestTask() {
-            @Override
-            public String getDescription() {
-                return "Create examples dir";
-            }
-
-            @Override
-            public void execute() throws Exception {
-                Files.createDirectories(new File(antSrcDir, "examples").toPath());
-            }
-        });
+        taskExecutor.addTask(new UnzipTask(zipFile, antDir));
         taskExecutor.addTask(new CopyDirectoryTask(antSrcDir, plutoDir));
-        MigrateAntToPlutoTask migrateAntToPlutoTask = new MigrateAntToPlutoTask(plutoBuildXml, plutoDir, "build.pluto."+projectName, false, debug, Arrays.asList("dist"));
+        MigrateAntToPlutoTask migrateAntToPlutoTask = new MigrateAntToPlutoTask(plutoBuildXml, plutoDir, "build.pluto."+projectName, false, debug);
         migrateAntToPlutoTask.setContinueOnError(true);
         migrateAntToPlutoTask.setCalculateStatistics(true);
         taskExecutor.addTask(migrateAntToPlutoTask);
@@ -145,7 +118,7 @@ public class SlickCorrectnessWithDockerTest {
         mounts.add(new Mount(antDir, new File("/share/test/")));
         mounts.add(new Mount(new File(System.getProperty("user.home")+"/.m2/"), new File("/share/m2/")));
 
-        taskExecutor.addTask(new DockerRunnerTask(antDir, projectName+"_Ant", new String(Files.readAllBytes(Paths.get(this.getClass().getResource("ant_command.txt").toURI()))), new File("/share/test/"+projectSourceName+"/"), mounts));
+        taskExecutor.addTask(new DockerRunnerTask(antDir, projectName+"_Ant", new String(Files.readAllBytes(Paths.get(this.getClass().getResource("ant_command.txt").toURI()))), new File("/share/test/"+projectSourceName+"/build/"), mounts));
 
 
         mounts = new ArrayList<>();
@@ -156,20 +129,13 @@ public class SlickCorrectnessWithDockerTest {
 
         String plutoRunCommand = new String(Files.readAllBytes(Paths.get(this.getClass().getResource("pluto_run_command.txt").toURI())));
         plutoRunCommand = CompileJavaTask.substituteVars(plutoRunCommand, new String[] {"<classpath>", "<projectname>", "<projectmain>"}, new String[]{classPathDocker, projectName, projectMainName});
-        taskExecutor.addTask(new DockerRunnerTask(plutoDir, projectName+"_Pluto", plutoRunCommand, new File("/share/test/"+projectSourceName+"/"), mounts));
+        taskExecutor.addTask(new DockerRunnerTask(plutoDir, projectName+"_Pluto", plutoRunCommand, new File("/share/test/"+projectSourceName+"/build/"), mounts));
 
-
-        ComparerTask comparerTask = new ComparerTask(new File(antSrcDir, "target"), new File(plutoSrcDir, "target"));
+        ComparerTask comparerTask = new ComparerTask(new File(antDir, "asmtools-6.0-build"), new File(plutoDir, "asmtools-6.0-build"));
         comparerTask.getDirectoryComparer().addFileComparer(new MD5FileComparer());
         comparerTask.getDirectoryComparer().addFileComparer(new UnzipFileComparer(comparerTask.getDirectoryComparer()));
         comparerTask.getDirectoryComparer().addFileComparer(new LineByLineFileComparer(Arrays.asList(new EqualLineComparer(), new JavaDocDateIgnoredLineComparer())));
         taskExecutor.addTask(comparerTask);
-
-        ComparerTask distComparerTask = new ComparerTask(new File(antSrcDir, "dist"), new File(plutoSrcDir, "dist"));
-        distComparerTask.getDirectoryComparer().addFileComparer(new MD5FileComparer());
-        distComparerTask.getDirectoryComparer().addFileComparer(new UnzipFileComparer(distComparerTask.getDirectoryComparer()));
-        distComparerTask.getDirectoryComparer().addFileComparer(new LineByLineFileComparer(Arrays.asList(new EqualLineComparer(), new JavaDocDateIgnoredLineComparer())));
-        taskExecutor.addTask(distComparerTask);
 
         taskExecutor.executeTasks();
     }
