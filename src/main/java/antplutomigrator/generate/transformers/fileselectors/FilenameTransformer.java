273140b9b2b59c3898aaf2a4d4a38474687490aa
Manuel Weiel <manuel@weiel.eu>
2017-10-12 13:43:18 +0200

    filename behaves like include (for now)

---
+++ src/main/java/antplutomigrator/generate/transformers/fileselectors/FilenameTransformer.java
@@ -0,0 +1,11 @@
+package antplutomigrator.generate.transformers.fileselectors;
+
+import antplutomigrator.generate.ElementGenerator;
+import antplutomigrator.generate.introspectionhelpers.AntIntrospectionHelper;
+import org.apache.tools.ant.UnknownElement;
+
+public class FilenameTransformer extends IncludeTransformer {
+    public FilenameTransformer(UnknownElement element, ElementGenerator elementGenerator, AntIntrospectionHelper introspectionHelper, String baseDir, String fileName) {
+        super(element, elementGenerator, introspectionHelper, baseDir, fileName);
+    }
+}
